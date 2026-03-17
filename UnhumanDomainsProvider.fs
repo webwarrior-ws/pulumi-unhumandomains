5c8cf734fe93382ddcebdfc79ad149dd54ee25f7
webwarrior-ws <reg@webwarrior.ws>
2026-03-17 13:17:39 +0100

    Implemented other operations

--- UnhumanDomainsProvider.fs
+++ UnhumanDomainsProvider.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Collections.Immutable
+open System.Collections.Generic
 open System.Linq
 open System.Net
 open System.Net.Http
@@ -42,17 +43,135 @@ type UnhumanDomainsProvider(managementToken: string) =
     interface IDisposable with
         override self.Dispose (): unit = 
             httpClient.Dispose()
-
-    member private self.GetDnsRecordPropertyString(dict: ImmutableDictionary<string, PropertyValue>, name: string) =
-        match dict.[name].TryGetString() with
-        | true, value -> value
-        | false, _ -> failwith $"No {name} property in {domainRecordResourceName}"
-
-    member private self.GetDnsRecordPropertyInt(dict: ImmutableDictionary<string, PropertyValue>, name: string) =
-        match dict.[name].TryGetNumber() with
-        | true, value -> int value
-        | false, _ -> failwith $"No {name} property in {domainRecordResourceName}"
     
+    static member RecordPropertyValueToDictionary (recordPropertyValue: PropertyValue) : IDictionary<string, obj> =
+        let _, object = recordPropertyValue.TryGetMap()
+        object 
+        |> Seq.map 
+            (fun item -> 
+                let value =
+                    if item.Value.Type = PropertyValueType.String then
+                        let _, strValue = item.Value.TryGetString()
+                        box strValue
+                    elif item.Value.Type = PropertyValueType.Number then
+                        let _, numValue = item.Value.TryGetNumber()
+                        box numValue
+                    else
+                        failwithf "Unexpected property value type: %A" item.Value.Type
+                item.Key, value)
+        |> dict
+    
+    member private self.AsyncGetDnsRecords(domainName: string): Async<Option<seq<IDictionary<string, PropertyValue>>>> =
+        async {
+            let! dnsRecordsResponse = 
+                httpClient.GetAsync($"{apiBaseUrl}/api/domains/{domainName}/dns")
+                |> Async.AwaitTask
+            let! responseBody = dnsRecordsResponse.Content.ReadAsStringAsync() |> Async.AwaitTask
+            if dnsRecordsResponse.StatusCode = HttpStatusCode.Conflict then
+                // custom nameservers
+                return None
+            elif not dnsRecordsResponse.IsSuccessStatusCode then
+                return failwith $"Error getting DNS records (status code {dnsRecordsResponse.StatusCode}):
+{responseBody}"
+            else
+                let data = JsonDocument.Parse(responseBody).RootElement.GetProperty "data"
+                let records = 
+                    data.GetProperty("records").EnumerateArray()
+                    |> Seq.map 
+                        (fun elem -> 
+                            elem.EnumerateObject() 
+                            |> Seq.map(fun object -> 
+                                let value = 
+                                    if object.Value.ValueKind = JsonValueKind.String then
+                                        object.Value.GetString() |> PropertyValue
+                                    elif object.Value.ValueKind = JsonValueKind.Number then
+                                        object.Value.GetInt32() |> PropertyValue
+                                    else
+                                        failwithf 
+                                            "Unexpected value kind of property %s: %A"
+                                                object.Name
+                                                object.Value.ValueKind
+                                object.Name, value)
+                            |> dict)
+                return Some records
+        }
+
+    member private self.AsyncGetNameservers(domainName: string): Async<seq<string>> =
+        async {
+            let! domainInfo = 
+                httpClient.GetStringAsync($"{apiBaseUrl}/api/domains/{domainName}/info")
+                |> Async.AwaitTask
+            let data = JsonDocument.Parse(domainInfo).RootElement.GetProperty "data"
+            let nameservers = 
+                data.GetProperty("nameservers").EnumerateArray()
+                |> Seq.map (fun elem -> elem.GetString())
+            return nameservers
+        }
+
+    member private self.AsyncSetDefaultNameservers (domainName: string): Async<unit> =
+        async {
+            let! useDefaultNameserversResponse = 
+                httpClient.PutAsync(
+                    $"{apiBaseUrl}/api/domains/{domainName}/nameservers",
+                    Json.JsonContent.Create {| useDefault = true |}
+                )
+                |> Async.AwaitTask
+            if not useDefaultNameserversResponse.IsSuccessStatusCode then
+                let! responseBody = useDefaultNameserversResponse.Content.ReadAsStringAsync() |> Async.AwaitTask
+                return failwith $"Error swithching to default nameservers (status code {useDefaultNameserversResponse.StatusCode}):
+{responseBody}"
+        }
+
+    member private self.AsyncSetDnsRecords (domainName: string) (records: seq<IDictionary<string,obj>>): Async<unit> =
+        async {
+            // first make sure that default nameservers are used
+            do! self.AsyncSetDefaultNameservers domainName
+
+            let payload = {| records = records |}
+            let! putDnsRecordsResponse = 
+                httpClient.PutAsync($"{apiBaseUrl}/api/domains/{domainName}/dns", Json.JsonContent.Create payload)
+                |> Async.AwaitTask
+            if not putDnsRecordsResponse.IsSuccessStatusCode then
+                let! responseBody = putDnsRecordsResponse.Content.ReadAsStringAsync() |> Async.AwaitTask
+                return failwith $"Error setting DNS records (status code {putDnsRecordsResponse.StatusCode}):
+{responseBody}"
+        }
+
+    member private self.AsyncSetNameservers (domainName: string) (nameservers: seq<string>): Async<unit> =
+        async {
+            let payload = {| nameservers = nameservers |}
+            let! putNameserversResponse = 
+                httpClient.PutAsync($"{apiBaseUrl}/api/domains/{domainName}/nameservers", Json.JsonContent.Create payload)
+                |> Async.AwaitTask
+            if not putNameserversResponse.IsSuccessStatusCode then
+                let! responseBody = putNameserversResponse.Content.ReadAsStringAsync() |> Async.AwaitTask
+                return failwith $"Error setting nameservers (status code {putNameserversResponse.StatusCode}):
+{responseBody}"
+        }
+
+    member private self.AsyncCreateOrUpdate (properties: ImmutableDictionary<string, PropertyValue>) =
+        async {
+            let _, domainName = properties.["domainName"].TryGetString()
+            let hasDnsRecords, dnsRecordsPropertyValue = properties.TryGetValue "records"
+            let hasNameservers, nameserversPropertyValue = properties.TryGetValue "nameservers"
+                
+            match hasDnsRecords, hasNameservers with
+            | true, false ->
+                let _, recordsArray = 
+                    dnsRecordsPropertyValue.TryGetArray()
+                let records =
+                    recordsArray
+                    |> Seq.map UnhumanDomainsProvider.RecordPropertyValueToDictionary
+                do! self.AsyncSetDnsRecords domainName records
+            | false, true ->
+                let _, nameserversArray = nameserversPropertyValue.TryGetArray()
+                let nameservers = 
+                    nameserversArray
+                    |> Seq.map (fun nameserver -> nameserver.TryGetString() |> snd)
+                do! self.AsyncSetNameservers domainName nameservers                    
+            | _ -> return failwith "Unreachable"
+        }
+
     override self.GetSchema (request: GetSchemaRequest, ct: CancellationToken): Task<GetSchemaResponse> = 
         let schema =
             let dnsRecordTypes = """{
@@ -196,7 +315,8 @@ type UnhumanDomainsProvider(managementToken: string) =
     member private self.AsyncCreate(request: CreateRequest): Async<CreateResponse> =
         async {
             if request.Type = domainRecordResourceName then
-                return failwith "Not yet implemented"
+                do! self.AsyncCreateOrUpdate request.Properties
+                return CreateResponse(Id = Guid.NewGuid().ToString(), Properties = request.Properties)
             else
                 return failwith $"Unknown resource type '{request.Type}'"
         }
@@ -207,8 +327,8 @@ type UnhumanDomainsProvider(managementToken: string) =
     member private self.AsyncUpdate(request: UpdateRequest): Async<UpdateResponse> =
         async {
             if request.Type = domainRecordResourceName then
-                let properties = request.Olds.AddRange request.News
-                return failwith "Not yet implemented"
+                do! self.AsyncCreateOrUpdate request.News
+                return UpdateResponse(Properties = request.News)
             else
                 return failwith $"Unknown resource type '{request.Type}'"
         }
@@ -219,7 +339,9 @@ type UnhumanDomainsProvider(managementToken: string) =
     member private self.AsyncDelete(request: DeleteRequest): Async<unit> =
         async {
             if request.Type = domainRecordResourceName then
-                return failwith "Not yet implemented"
+                let _, domainName = request.Properties.["domainName"].TryGetString()
+                do! self.AsyncSetDefaultNameservers domainName
+                do! self.AsyncSetDnsRecords domainName ImmutableArray.Empty
             else
                 failwith $"Unknown resource type '{request.Type}'"
         }
@@ -230,7 +352,28 @@ type UnhumanDomainsProvider(managementToken: string) =
     member private self.AsyncRead (request: ReadRequest) : Async<ReadResponse> =
         async {
             if request.Type = domainRecordResourceName then
-                return failwith "Not yet implemented"
+                let _, domainName = request.Properties.["domainName"].TryGetString()
+                let! getRecordsResult = self.AsyncGetDnsRecords domainName
+                
+                match getRecordsResult with
+                | None ->
+                    let! nameservers = self.AsyncGetNameservers domainName
+                    let properties = 
+                        dict [
+                            "domainName", PropertyValue domainName
+                            "nameservers", nameservers |> Seq.map PropertyValue |> ImmutableArray.CreateRange |> PropertyValue
+                        ]
+                    return ReadResponse(Id = request.Id, Properties = properties)
+                | Some records ->
+                    let recordPropertyValues =
+                        records 
+                        |> Seq.map (fun record -> record.ToImmutableDictionary() |> PropertyValue)
+                    let properties = 
+                        dict [
+                            "domainName", PropertyValue domainName
+                            "records", recordPropertyValues |> ImmutableArray.CreateRange |> PropertyValue
+                        ]
+                    return ReadResponse(Id = request.Id, Properties = properties)
             else
                 return failwith $"Unknown resource type '{request.Type}'"
         }
